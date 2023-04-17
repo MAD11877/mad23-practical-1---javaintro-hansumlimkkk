0a9f40d7b0e274acc60ffe69dbeaab6dcf26a066
hansumlimkkk <130948581+hansumlimkkk@users.noreply.github.com>
2023-04-17 12:07:48 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -30,3 +30,36 @@ public class Question5
     
   }
 }
+
+import java.util.Scanner;
+
+class Main 
+{
+   public static void main(String[] args) {
+     List<int> list1 = new List<int>();
+     List<int> list2 = new List<int>();
+     Scanner in = new Scanner(System.in);
+     int times = scanner.NextInt();
+     for (int i = times; i>=1;i--){
+       Scanner in1 = new Scanner(System.in);
+       int number = scanner.NextInt();
+       list1.add(number);
+     }
+     for (int j = 0; j<= list1.size();j-- ){
+       int num = list1.get(j);
+       int count = 0;
+       for (int k = 0; k<= list1.size();k-- ){
+         count = 0;
+         string val = list1.get(k);
+         if (val == num){
+           count++;
+         }
+       } 
+        list2.add(count);
+    }
+    
+      
+      
+    
+}
+}
